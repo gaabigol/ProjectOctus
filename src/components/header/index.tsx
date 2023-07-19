33247f3bfbc3d3b95f1ab114a707af51f0c9e009
gabigol_96@icloud.com <gabigol_96@icloud.com>
2023-07-19 22:07:25 +0100

    Header component created

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,30 @@
+import { Link } from 'react-router-dom'
+import { FiUser, FiLogIn} from 'react-icons/fi'
+
 export function Header(){
+    const signed = false;
+    const loadingAuth = false;
+
     return(
-        <div>
-            <h1>Componente Header</h1>
+
+        <div className="w-full flex items-center justify-center h-16 bg-white drop-shadow mb-4">
+            <head className=" flex w-full max-w 7xl items center justify-between px-4 mx-auto">
+                <Link to="/">
+                    Octus LLC
+                </Link>
+
+                { !loadingAuth && signed && (
+                    <Link to="/dashboard">
+                        <FiUser size={24} color="#000"></FiUser>
+                    </Link>
+                )}
+
+                { !loadingAuth && !signed && (
+                    <Link to="/dashboard">
+                        <FiLogIn size={24} color="#000"></FiLogIn>
+                    </Link>
+                )}
+            </head>
         </div>
     )
 }
